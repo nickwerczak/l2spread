48fc50e4ced12fbdf5bd48fa57bf87459fd7bc90
Nick Werczak <nick@tradeblock.com>
2023-06-09 09:49:56 -0400

    Update

--- src/server.rs
+++ src/server.rs
@@ -2,60 +2,22 @@ use tonic::transport::Server;
 use tonic::{Request, Response, Status};
 
 use tokio::sync::mpsc;
-
-use futures_core::Stream;
-//use async_stream::stream;
-
-//use futures_util::pin_mut;
-//use futures_util::stream::StreamExt;
-
+use tokio_stream::wrappers::UnboundedReceiverStream;
 
 mod orderbook;
 use orderbook::orderbook_aggregator_server::{OrderbookAggregatorServer, OrderbookAggregator};
 use orderbook::{Empty, Summary, Level};
 
-use std::pin::Pin;
-use std::task::{Context, Poll};
-
 #[derive(Default)]
 pub struct MyServer {}
 
+
 #[tonic::async_trait]
-impl Stream for BookSummaryStream {
-    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>)
-        -> Poll<Option<()>>
-    {
-        if self.rem == 0 {
-            // No more delays
-            return Poll::Ready(None);
-        }
-
-        match Pin::new(&mut self.delay).poll(cx) {
-            Poll::Ready(_) => {
-                self.rem -= 1;
-                Poll::Ready(Some(()))
-            }
-            Poll::Pending => Poll::Pending,
-        }
-    }
-}
-
 impl OrderbookAggregator for MyServer {
 
-//    let s = Stream! {
-//        let bids = vec![Level{exchange: "nick".to_string(), price: 1.0, amount: 1.0 }];
-//        let asks = vec![Level{exchange: "nick".to_string(), price: 1.1, amount: 1.1 }];
-//        let quotes = vec![Summary{ spread: 0.1, bids: bids, asks: asks}, Summary{ spread: 0.1, bids: bids, asks: asks}];
-//        return quotes;
-//    }
+    type BookSummaryStream = UnboundedReceiverStream<Result<Summary, tonic::Status>>;
 
-    //type BookSummaryStream: futures_core::Stream<Item=Result<Summary, Status>>
-    type BookSummaryStream: Stream<Item=Result<Summary, Status>>
-        + Send
-        + Sync
-        + 'static = mpsc::Receiver<Result<Summary, Status>>;
-
-    async fn book_summary(&self, request : Request<Empty>) -> Result<Response<Self::BookSummaryStream>, Status> {
+    async fn book_summary(&self, request : Request<Empty>) -> Result<Response<Self::BookSummaryStream>, tonic::Status> {
         let (mut tx, rx) = mpsc::channel(4);
 
         let bids = vec![Level{exchange: "nick".to_string(), price: 1.0, amount: 1.0 }];
@@ -71,11 +33,15 @@ impl OrderbookAggregator for MyServer {
                     asks: quote.asks,
                 })).await.unwrap();
             }
+//            tx.send(Ok(summary_stream())).await.unwrap();
         });
 
-        Ok(Response::new(rx))
+        let stream = Self::BookSummaryStream::new(rx);
+        //Ok(Response::new(Box::pin(stream)))
+        Ok(Response::new(stream))
     }
 }
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
