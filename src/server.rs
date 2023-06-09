ac4d228d39b4674028c858cbcbb1d7b1c65ebd03
Nick Werczak <nick@tradeblock.com>
2023-06-09 15:41:58 -0400

    Update

--- src/server.rs
+++ src/server.rs
@@ -8,9 +8,15 @@ mod orderbook;
 use orderbook::orderbook_aggregator_server::{OrderbookAggregatorServer, OrderbookAggregator};
 use orderbook::{Empty, Summary, Level};
 
-#[derive(Default)]
+#[derive(Debug)]
 pub struct MyServer {}
 
+impl Default for MyServer {
+    fn default() -> Self {
+        MyServer {
+        }
+    }
+}
 
 #[tonic::async_trait]
 impl OrderbookAggregator for MyServer {
@@ -18,27 +24,33 @@ impl OrderbookAggregator for MyServer {
     type BookSummaryStream = UnboundedReceiverStream<Result<Summary, tonic::Status>>;
 
     async fn book_summary(&self, request : Request<Empty>) -> Result<Response<Self::BookSummaryStream>, tonic::Status> {
-        let (mut tx, rx) = mpsc::channel(4);
+        //let (mut tx, rx) = mpsc::channel(4);
+        let (tx, rx) = mpsc::unbounded_channel();
 
         let bids = vec![Level{exchange: "nick".to_string(), price: 1.0, amount: 1.0 }];
         let asks = vec![Level{exchange: "nick".to_string(), price: 1.1, amount: 1.1 }];
-        let quotes = vec![Summary{ spread: 0.1, bids: bids, asks: asks}, Summary{ spread: 0.1, bids: bids, asks: asks}];
+        let quotes = vec![Summary{ spread: 0.1, bids: bids.clone(), asks: asks.clone()}, Summary{ spread: 0.1, bids: bids.clone(), asks: asks.clone()}];
 
         tokio::spawn(async move {
             for q in quotes {
                 let quote = q.clone();
-                tx.send(Ok(Summary {
+                tx.send(Summary {
                     spread: quote.spread,
                     bids: quote.bids,
                     asks: quote.asks,
-                })).await.unwrap();
+                //}).await.unwrap();
+                });
             }
 //            tx.send(Ok(summary_stream())).await.unwrap();
         });
 
-        let stream = Self::BookSummaryStream::new(rx);
-        //Ok(Response::new(Box::pin(stream)))
-        Ok(Response::new(stream))
+        //let stream = UnboundedReceiverStream<Result<Summary, tonic::Status>>::new(rx);
+        //Ok(Response::new(Box::pin(stream) as Self::BookSummaryStream, ))
+        //let stream: Self::BookSummaryStream = new Self::BookSummaryStream();
+        //let stream: Self::BookSummaryStream;
+        //Ok(Response::new(stream))
+        let stream = UnboundedReceiverStream::new(rx);
+        Ok(Response::new(Box::pin(stream) as Self::BookSummaryStream))
     }
 }
 
